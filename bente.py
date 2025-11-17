44b802b3ccb5fc2b68d99a95cf333593de186158
Bente Bruurs <bentebruurs@acknowledge.nl>
2025-11-17 10:34:39 +0100

    bente.py aangemaakt

--- bente.py
+++ bente.py
@@ -1,3 +1,14 @@
+from connect_with_db import get_cursor
+from flask import jsonify
+
 def start():
-    print("Hoi, Bente hier!")
-    return "Bente"
+    print("Het is gelukt!")
+
+    myconn, mycursor = get_cursor()
+    mycursor.execute("""SELECT Eigenaar_of_huurder, Perioden, `Tevredenheid met de huidige woning (%)`, `Tevredenheid met de huidige woonomgeving (%)`
+FROM urbanmythdb.woontevredenheid""")
+    rows = mycursor.fetchall()
+    keys = [i[0] for i in mycursor.description]
+    data = [dict(zip(keys, row)) for row in rows]
+
+    return jsonify(data)
